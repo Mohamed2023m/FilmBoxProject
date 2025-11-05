eab7da791994ae107bd65b23961d46bc52c14a65
Mohamed <10514400@ucn.dk>
2025-11-05 14:02:16 +0100

    Initial commit: FilmBox MAUI app and API

---
+++ FilmBox/MauiProgram.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+
+namespace FilmBox
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            // -------------------------------
+            // Add HttpClient for API
+            // -------------------------------
+            builder.Services.AddHttpClient("FilmBoxAPI", client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/"); // your WebAPI URL
+            });
+
+      
+
+            return builder.Build();
+        }
+    }
+}
