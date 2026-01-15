bc8f1613e47f921d305cead7ccf374dde634965d
Mohamed <10514400@ucn.dk>
2026-01-15 22:06:06 +0100

    accidentally deleted and added folders

---
+++ FilmBox.API/DTOs/Converters/DtoConverter.cs
@@ -0,0 +1,74 @@
+﻿using FilmBox.Api.DTOs;
+using FilmBox.Api.DTOs.GetDTOs;
+using FilmBox.Api.DTOs.PostDTOs;
+using FilmBox.Api.Models;
+using System.Collections.Generic;
+
+namespace WebApi.DTOs.Converters
+{
+    /// <summary>
+    /// Tool class for storing extension methods for converting DTOs to Model objects and back
+    /// </summary>
+    public static class DtoConverter
+    {
+        #region User conversion methods
+        public static UserDto ToDto(this User userToConvert)
+        {
+            var userDto = new UserDto();
+            userToConvert.CopyPropertiesTo(userDto);
+            return userDto;
+        }
+
+        public static User FromDto(this UserDto userDtoToConvert)
+        {
+            var user = new User();
+            userDtoToConvert.CopyPropertiesTo(user);
+            return user;
+        }
+
+        public static IEnumerable<UserDto> ToDtos(this IEnumerable<User> usersToConvert)
+        {
+            foreach (var user in usersToConvert)
+            {
+                yield return user.ToDto();
+            }
+        }
+
+        public static IEnumerable<User> FromDtos(this IEnumerable<UserDto> userDtosToConvert)
+        {
+            foreach (var userDto in userDtosToConvert)
+            {
+                yield return userDto.FromDto();
+            }
+        }
+        #endregion
+
+
+        #region Review conversion methods
+        public static ReviewDto ToDto(this Review reviewToConvert)
+        {
+            var reviewDto = new ReviewDto();
+            reviewToConvert.CopyPropertiesTo(reviewDto);
+            return reviewDto;
+        }
+
+        public static Review FromCreateDto(this ReviewCreateDto reviewDtoToConvert)
+        {
+            var review = new Review();
+            reviewDtoToConvert.CopyPropertiesTo(review);
+            return review;
+        }
+
+        public static IEnumerable<ReviewDto> ToDtos(this IEnumerable<Review> reviewsToConvert)
+        {
+            foreach (var review in reviewsToConvert)
+            {
+                yield return review.ToDto();
+            }
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ FilmBox.API/DTOs/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FilmBox.Api.DTOs
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = "User";
+        public int UserId { get; internal set; }
+    }
+}
